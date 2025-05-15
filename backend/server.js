80762434d8d66584eeeed44cd8e1b19f40341e2f
Nikhil Sharma <xharmanikk9876@gmail.com>
2025-05-15 21:12:09 +0530

    Updated backend server config

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const menuRoutes = require('./routes/menuRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
+app.use('/menu', menuRoutes);
+app.use('/contact', contactRoutes);
+app.use('/order', orderRoutes);
+
 dotenv.config();
 
 const app = express();
